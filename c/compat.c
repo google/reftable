573a506a99ba21f2e166aefbe44f55e3a8c5dda0
Han-Wen Nienhuys <hanwen@google.com>
2020-03-26 19:25:54 +0100

    C: document compat.c purpose

--- c/compat.c
+++ c/compat.c
@@ -4,7 +4,10 @@ Copyright 2020 Google LLC
 Use of this source code is governed by a BSD-style
 license that can be found in the LICENSE file or at
 https://developers.google.com/open-source/licenses/bsd
+
 */
+
+/* compat.c - compatibility functions for standalone compilation */
 
 #include "system.h"
 #include "basics.h"
